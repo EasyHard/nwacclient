2b91a25db546ef8c74ef5b4873327ccd735d9560
Zhiyou Liu <zhiyliu@microsoft.com>
2021-02-24 17:08:52 -0800

    missing file

---
+++ src/DataLogger.hs
@@ -0,0 +1,6 @@
+module DataLogger
+    ( 
+        DataLogger(..)
+    ) where
+
+data DataLogger = SnoqualmiePass | SnoqualmiePassDodgeRidge | StevensPass | MtBaker | CrystalBase | CrystalGreenValley | TimberlineLodge | MissionRidgeMidMountain deriving (Show)
